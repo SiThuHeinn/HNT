dc2598dea68fe2b67fe7ca477df7590c31161dda
Sithuhein <sithu.98.hein@gmail.com>
2022-07-22 22:55:42 +0630

    inital commit

---
+++ app/src/main/java/com/sithuhein/mm/hanacodetest/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sithuhein.mm.hanacodetest.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sithuhein.mm.hanacodetest.R.layout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
